033b2ca41338e8b43c08aef926693c399de5c27b
Hakutoshiro <ayushdoshi782@gmail.com>
2024-02-03 08:27:44 +0530

    minor changes in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -59,7 +59,7 @@ app.post('/login', async (req, res) => {
             const passOk =  bcrypt.compareSync(password, userDoc.password)
             if(passOk){
                 jwt.sign({email:userDoc.email,id:userDoc._id,name:userDoc.name},jwtSecret,{},(err,token) => {
-                    if(err) throw err;
+                    if(err) res.json('Unable to login');
                     res.cookie('token', token).json(userDoc)
                 })
             }
@@ -75,9 +75,11 @@ app.post('/login', async (req, res) => {
 
 app.get('/profile',(req,res)=>{
     const {token} = req.cookies
+    
     if(token){
         jwt.verify(token,jwtSecret,{},async (err,data) =>{
-            if(err)throw err;
+            if(err)res.json("Data retreival failed('/profile')");
+
             try {
                 const {name,email,_id} =await User.findById(data.id);
                 res.json({name,email,_id});
@@ -99,31 +101,34 @@ app.post('/addtask',async (req,res)=>{
     let UserID ='';
     if(token){
         jwt.verify(token,jwtSecret,{},async (err,data) =>{
-            if(err)throw err;
+            if(err)res.json("Unable to add task('/addtask')");
             UserID=data.id;
-        })
-    console.log({title,description,date})
-    }
-    try {
-        const params = {
-            TableName: 'Tasks',
-            Item: {
-                'UserID': UserID,
-                'TaskID ' : "TaskID_"+Math.random(),
-                'title': title,
-                'description': description,
-                'date': date,
-                'status': false,
+            if(UserID){
+                try {
+                    const params = {
+                        TableName: 'Tasks',
+                        Item: {
+                            'UserID': UserID,
+                            'TaskID ' : "TaskID_"+Math.random(),
+                            'title': title,
+                            'description': description,
+                            'date': date,
+                            'status': false,
+                        }
+                    };
+                    await docClient.put(params, (err,data)=>{
+                        if(err)res.json("Unable to add task('/addtask-aws')");
+                        res.json('Task Add')
+                    })
+                    
+                } catch (error) {
+                    res.status(422).json('err')
+                }
             }
-        };
-        await docClient.put(params, (err,data)=>{
-            if(err)throw err;
-            res.json('Task Add')
         })
-        
-    } catch (error) {
-        res.status(422).json('err')
+    
     }
+    
 })
 
 app.get('/dataretreival',async (req,res)=>{
@@ -131,30 +136,32 @@ app.get('/dataretreival',async (req,res)=>{
     let UserID ='';
     if(token){
         jwt.verify(token,jwtSecret,{},async (err,data) =>{
-            if(err)throw err;
+            if(err)res.json("data retreival failed('/dataretreival')");
             UserID=data.id;
+            if(UserID){
+                try {
+                    const params ={
+                        TableName:'Tasks',
+                        FilterExpression:'#UserID=:UserID',
+                        ExpressionAttributeNames:{
+                            '#UserID':'UserID',
+                        },
+                        ExpressionAttributeValues:{
+                            ':UserID':UserID,
+                        }
+                    }
+                    await docClient.scan(params,(err,data)=>{
+                        if(err) res.json("data retreival failed('/dataretreival-aws')") ;                        
+                        res.json(data.Items)
+                    })
+                } catch (error) {
+                    res.status(422).json('err');
+                }
+            }
         })
         
     }
-    try {
-        const params ={
-            TableName:'Tasks',
-            FilterExpression:'#UserID=:UserID',
-            ExpressionAttributeNames:{
-                '#UserID':'UserID',
-            },
-            ExpressionAttributeValues:{
-                ':UserID':UserID,
-            }
-        }
-        await docClient.scan(params,(err,data)=>{
-            if(err) throw err;
-            
-            res.json(data.Items)
-        })
-    } catch (error) {
-        res.status(422).json('err');
-    }
+    
 })
 
 app.post('/taskdone',async (req, res)=>{
